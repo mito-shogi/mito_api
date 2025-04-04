c96bbde58612e9f702793d549cdc217091567888
ultemica <ultemica@proton.me>
2025-04-04 14:41:11 +0900

    feat: first release (#7)
    * refactor(bindings): remove unused KV namespaces and related code
    
    * chore(config): update d1 database ID in wrangler.toml
    
    * chore(config): update project name in wrangler.toml

--- src/utils/bindings.ts
+++ src/utils/bindings.ts
@@ -3,12 +3,6 @@ export type Bindings = {
   WARS_USER_ID: string
   WARS_SECRET: string
   WARS_AUTHENTICITY_TOKEN: string
-  DISCORD_WEBHOOK_URL: string
-  APP_REDIRECT_URI: string
-  APP_JWT_SECRET: string
   CACHE: KVNamespace
-  USERS: KVNamespace
-  GAMES: KVNamespace
-  CSA: KVNamespace
   DB: D1Database
 }

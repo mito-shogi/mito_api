2d1fc6d70292bc265d729abb411b59a8b7a44791
tkgstrator <nasawake.am@gmail.com>
2025-04-04 03:26:39 +0000

    feat(migrations): add user and game tables with schema and migration steps

--- migrations/0001_create_user_table.sql
+++ migrations/0001_create_user_table.sql
@@ -1,1 +1,28 @@
--- Migration number: 0001 	 2025-04-04T02:57:43.452Z
+-- CreateTable
+CREATE TABLE "User" (
+    "user_id" TEXT NOT NULL PRIMARY KEY,
+    "rank" INTEGER NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "game_id" TEXT NOT NULL PRIMARY KEY,
+    "mode" TEXT NOT NULL,
+    "rule" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "handicap" INTEGER,
+    "kif" TEXT,
+    "play_time" DATETIME NOT NULL,
+    "position" TEXT,
+    "result" TEXT NOT NULL,
+    "reason" TEXT,
+    "black_id" TEXT NOT NULL,
+    "white_id" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "Game_black_id_fkey" FOREIGN KEY ("black_id") REFERENCES "User" ("user_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Game_white_id_fkey" FOREIGN KEY ("white_id") REFERENCES "User" ("user_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
